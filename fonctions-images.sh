5c3796366e4c71ea4bf970fdbef83d484b42ec50
Jp-Gigo <jpg-leboss@mail.fr>
2021-01-23 18:47:43 +0100

    Modification mineure

--- fonctions-images.sh
+++ fonctions-images.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#------------------------------------------------------------------------------#
-# Ensemble de fonctions pour modifier et optimiser des Images.
-# Origine des fonctions
-# https://css-tricks.com/converting-and-optimizing-images-from-the-command-line/
-# jpg.
-#
-#
-# Necessite l 'installation de imagemagik, webp jpegoptim, optipng
-# apt-get update
-# apt-get install imagemagick webp jpegoptim optipng
-# apt install npm
-# npm install -g svgexport svgo (j'ai eu des erreurs lors de l'install de svgexport, a tester)
-# https://github.com/shakiba/svgexport
-# https://github.com/svg/svgo
-# Il sera bien de créer un menu pour faciliter l'usage de ces fonctions.
-#------------------------------------------------------------------------------#
 
 ### Organiser les images ###
  # OK, nous sommes techniquement configurés.
